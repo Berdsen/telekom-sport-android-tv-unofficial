96fdde70a8f4940be53416a7f0f69e3a0e8b193f
Berdsen <berdsen.home@gmail.com>
2017-09-28 21:42:15 +0200

    made the login part read the values. POST and saving session is missing

--- TelekomSportUnofficial/src/main/java/de/berdsen/telekomsport_unofficial/services/SessionService.java
+++ TelekomSportUnofficial/src/main/java/de/berdsen/telekomsport_unofficial/services/SessionService.java
@@ -1,12 +1,27 @@
 package de.berdsen.telekomsport_unofficial.services;
 
 import android.os.AsyncTask;
+import android.text.Html;
+import android.text.Spanned;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -39,16 +54,60 @@ public class SessionService {
         }
 
         private boolean executeLogin() {
-            // load html and session stuff
             try {
                 URL url = new URL(constants.getLoginUrl());
 
                 HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
-                connection.connect();
+                //connection.setAllowUserInteraction(false);
+                //connection.setInstanceFollowRedirects(true);
+                //connection.setRequestMethod("GET");
+
+                connection.setRequestProperty("User-Agent", System.getProperty("http.agent"));
+                connection.setRequestProperty("Accept-Encoding", "gzip");
+
+                // execute request and get response code
+                int res = connection.getResponseCode();
+
                 // we need the Session cookies
                 List<String> cookies = connection.getHeaderFields().get("Set-Cookie");
 
-                Map<String, List<String>> requestProperties = connection.getRequestProperties();
+                String html = "";
+
+                if (res == HttpURLConnection.HTTP_OK) {
+                    // we accepted gzip, so we need to extract it
+                    InputStream in = new GZIPInputStream(connection.getInputStream());
+
+                    // TODO: use encoding from html header
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"), 8);
+
+                    // TODO: extract this to utils class
+                    StringBuilder sb = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        sb.append(line).append("\n");
+                    }
+                    in.close();
+                    html = sb.toString();
+                } else {
+                    // TODO: error handling
+                    int error = res;
+                }
+
+                // parse html to document
+                Document loginPage = Jsoup.parse(html);
+
+                Element input = loginPage.getElementById("login");
+
+                // get the requrired parameters
+                Map<String, String> postParameter = new HashMap<>();
+
+                if (input != null) {
+                    List<Element> listOfInputElements = input.getElementsByTag("input");
+
+                    for (Element e : listOfInputElements) {
+                        postParameter.put(e.attr("name"), e.attr("value"));
+                    }
+                }
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
