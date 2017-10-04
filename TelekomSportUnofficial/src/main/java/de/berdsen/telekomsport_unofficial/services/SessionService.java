cc4a68967c398ec0091a5e38dfd31549d8fd54cf
Berdsen <berdsen.home@gmail.com>
2017-10-04 15:32:17 +0200

    more POSTing

--- TelekomSportUnofficial/src/main/java/de/berdsen/telekomsport_unofficial/services/SessionService.java
+++ TelekomSportUnofficial/src/main/java/de/berdsen/telekomsport_unofficial/services/SessionService.java
@@ -1,8 +1,6 @@
 package de.berdsen.telekomsport_unofficial.services;
 
 import android.os.AsyncTask;
-import android.text.Html;
-import android.text.Spanned;
 import android.text.TextUtils;
 
 import org.jsoup.Jsoup;
@@ -10,19 +8,19 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.HttpCookie;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,16 +38,20 @@ import de.berdsen.telekomsport_unofficial.utils.ParseUtils;
 public class SessionService {
 
     private TelekomApiConstants constants;
+    private CookieManager cookieManager;
 
     public SessionService(TelekomApiConstants constants) {
         this.constants = constants;
+
+        // see https://developer.android.com/reference/java/net/HttpURLConnection.html
+        this.cookieManager = new CookieManager(null, CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(cookieManager);
+        cookieManager.getCookieStore().removeAll();
     }
 
     public void loginAsync() {
-
-        LoginTask lt = new LoginTask();
+        LoginTask lt = new LoginTask(cookieManager);
         lt.execute();
-
     }
 
     private class LoginTask extends AsyncTask<Void, Void, Boolean> {
@@ -60,7 +62,7 @@ public class SessionService {
         private final String ENCODING_GZIP = "gzip";
         private final String USER_AGENT = "User-Agent";
         private final String COOKIE_HEADER_FIELD = "Set-Cookie";
-        private final String USER_AGENT_VALUE = System.getProperty("http.agent");
+        private String USER_AGENT_VALUE = "";
 
         private final String INPUT_ELEMENT_ID = "login";
         private final String INPUT_TAG = "input";
@@ -70,26 +72,62 @@ public class SessionService {
         private final String KEY_USERNAME = "pw_usr";
         private final String KEY_SUBMIT = "pw_submit";
 
+        private CookieManager cookieManager;
+
+        public LoginTask(CookieManager cookieManager) {
+            this.cookieManager = cookieManager;
+        }
+
         @Override
         protected Boolean doInBackground(Void... voids) {
+            setUserAgentValue();
+
             return executeLogin();
         }
 
-        private HttpsURLConnection createConnection() throws IOException {
-            URL url = new URL(constants.getLoginUrl());
+        private void setUserAgentValue() {
+            String chrome_version = "Chrome/59.0.3071.115";
+            String base = "Mozilla/5.0 ";
+            base += "(Windows NT 6.1; WOW64) ";
+            base += "AppleWebKit/537.36 (KHTML, like Gecko) ";
+            base += "%CH_VER% Safari/537.36".replace("%CH_VER%", chrome_version);
+
+            USER_AGENT_VALUE = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36";
+        }
+
+        private HttpsURLConnection createConnection(String connectionUrl) throws IOException {
+            URL url = new URL(connectionUrl);
 
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+
+            Map<String, List<String>> requestProperties = connection.getRequestProperties();
 
             connection.setRequestProperty(USER_AGENT, USER_AGENT_VALUE);
             connection.setRequestProperty(ACCEPT_ENCODING, ENCODING_GZIP);
             connection.setRequestProperty(ACCEPT_CHARSET, ENCODING_UTF8);
+
+            //connection.setRequestProperty("User-agent", System.getProperty("http.agent"));
+
+            connection.setUseCaches (false);
 
             return connection;
         }
 
         private boolean executeLogin() {
             try {
-                HttpsURLConnection connection = createConnection();
+                if (checkLoggedIn()) return true;
+
+                /*
+                HttpsURLConnection firstConnection = createConnection("https://www.telekomsport.de");
+                int firstRes = firstConnection.getResponseCode();
+                Map<String, List<String>> headerFields1 = firstConnection.getHeaderFields();
+
+                List<String> cookies = headerFields1.get(COOKIE_HEADER_FIELD);
+                */
+
+
+
+                HttpsURLConnection connection = createConnection(constants.getLoginUrl());
 
                 // execute request and get response code
                 int res = connection.getResponseCode();
@@ -97,6 +135,8 @@ public class SessionService {
                 // we need the Session cookies
                 Map<String, List<String>> headerFields = connection.getHeaderFields();
                 List<String> cookies = headerFields.get(COOKIE_HEADER_FIELD);
+
+                //loadResponseCookies(connection, this.cookieManager);
 
                 String html = "";
 
@@ -142,6 +182,10 @@ public class SessionService {
 
                 ExecutePost(cookies, postParameter);
 
+                // loadResponseCookies(connection, cookieManager);
+
+                return checkLoggedIn();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -149,7 +193,155 @@ public class SessionService {
             return false;
         }
 
+        private boolean checkLoggedIn() throws IOException {
+            HttpsURLConnection connection = createConnection(constants.getBaseUrl());
+            populateCookieHeaders(connection);
+
+            // execute request and get response code
+            int res = connection.getResponseCode();
+
+            if (res == HttpURLConnection.HTTP_OK) {
+                // we accepted gzip, so we need to extract it
+                InputStream in = new GZIPInputStream(connection.getInputStream());
+
+                String html = ParseUtils.readHtmlFromInputStream(in, ENCODING_UTF8);
+
+                // parse html to document
+                Document pageContent = Jsoup.parse(html);
+
+                Elements logout = pageContent.getElementsByClass("logout");
+
+                if (logout != null && logout.size() > 0) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void loadResponseCookies(HttpURLConnection conn) {
+
+            //do nothing in case a null cokkie manager object is passed
+            if (this.cookieManager == null || conn == null){
+                return;
+            }
+
+            List<String> cookiesHeader = conn.getHeaderFields().get( COOKIE_HEADER_FIELD );
+            if (cookiesHeader != null) {
+                for (String cookieHeader : cookiesHeader) {
+                    List<HttpCookie> cookies;
+                    try {
+                        cookies = HttpCookie.parse(cookieHeader);
+                    } catch (NullPointerException e) {
+                        continue;
+                    }
+
+                    if (cookies != null) {
+                        for (HttpCookie cookie : cookies) {
+                            // do something
+                        }
+                        if (cookies.size() > 0) {
+                            try {
+                                this.cookieManager.getCookieStore().add(new URI("http://www.telekomsport.de"), HttpCookie.parse(cookieHeader).get(0));
+                            } catch (URISyntaxException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void populateCookieHeaders(HttpURLConnection conn) {
+
+            if (this.cookieManager != null) {
+                //getting cookies(if any) and manually adding them to the request header
+                List<HttpCookie> cookies = this.cookieManager.getCookieStore().getCookies();
+
+                if (cookies != null) {
+                    if (cookies.size() > 0) {
+                        for (HttpCookie cookie : cookies) {
+                        }
+
+                        //adding the cookie header
+
+                        conn.setRequestProperty("Cookie", TextUtils.join(";", cookies));
+                    }
+                }
+            }
+        }
+
         private void ExecutePost(List<String> cookies, Map<String, String> postParameter) throws IOException {
+
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String,String> param : postParameter.entrySet()) {
+                if (param.getValue() == "") continue;
+
+                if (postData.length() != 0) postData.append('&');
+
+                postData.append(URLEncoder.encode(param.getKey(), ENCODING_UTF8));
+                postData.append('=');
+                postData.append(URLEncoder.encode(String.valueOf(param.getValue()), ENCODING_UTF8));
+            }
+            String urlParameters = postData.toString();
+
+            StringBuilder cookieData = new StringBuilder();
+            for (String param : cookies) {
+
+                if (cookieData.length() != 0) cookieData.append(";");
+                String[] value = param.split(";");
+                cookieData.append(value[0]);
+                cookieData.append(value[1]);
+
+            }
+            String cookieParameters = cookieData.toString();
+
+            urlParameters += "&headto=https://www.telekomsport.de/";// [{"key":"headto","value":"https://www.telekomsport.de/","description":""}]
+
+            HttpsURLConnection connection = (HttpsURLConnection)(new URL(constants.getLoginEndpoint()).openConnection());
+            connection.setRequestProperty(USER_AGENT, USER_AGENT_VALUE);
+            connection.setRequestProperty(ACCEPT_ENCODING, ENCODING_GZIP);
+            connection.setRequestProperty(ACCEPT_CHARSET, ENCODING_UTF8);
+            // connection.setRequestProperty("Cookie", cookieParameters);
+
+            populateCookieHeaders(connection);
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+            //connection.setRequestProperty("Content-Language", "en-US");
+            connection.setUseCaches (false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+
+            DataOutputStream wr = new DataOutputStream ( connection.getOutputStream () );
+            wr.writeBytes (urlParameters);
+            wr.flush ();
+            wr.close ();
+
+            connection.connect();
+
+            Map<String, List<String>> headerFields = connection.getHeaderFields();
+
+            String html = "";
+
+            int res = connection.getResponseCode();
+
+            if (res == HttpURLConnection.HTTP_OK) {
+                html = ParseUtils.readHtmlFromInputStream(new GZIPInputStream(connection.getInputStream()), ENCODING_UTF8);
+            } else {
+                // TODO: error handling
+                int error = res;
+            }
+
+            Document loginPage = Jsoup.parse(html);
+
+            Elements title = loginPage.getElementsByTag("title");
+
+            loadResponseCookies(connection);
+        }
+
+        private void ExecutePost_Old(List<String> cookies, Map<String, String> postParameter) throws IOException {
 
             StringBuilder postData = new StringBuilder();
             for (Map.Entry<String,String> param : postParameter.entrySet()) {
